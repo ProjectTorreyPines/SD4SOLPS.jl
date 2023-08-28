434fdf930765aa7fab05244d710a46f30cdb794d
David Eldon <eldond@fusion.gat.com>
2023-08-28 13:29:19 -0700

    Add function for finding input files for preparing a case

--- src/SD4SOLPS.jl
+++ src/SD4SOLPS.jl
@@ -1,10 +1,59 @@
 module SD4SOLPS
 
-import SOLPS2IMAS
 import OMAS
+import SOLPS2IMAS
+#import GGDUtils
 
 include("$(@__DIR__)/supersize_profile.jl")
 
 greet() = print("Hello World!")
+
+"""
+    find_files_in_allowed_folders()
+
+Searches a list of allowed folders for a set of filenames that will provide information
+about the SOLPS case. Returns a list of filenames with complete paths.
+
+Example:
+SD4SOLPS.find_files_in_allowed_folders("<your samples folder>/D3D_Ma_184833_03600", eqdsk_file="g184833.03600")
+"""
+function find_files_in_allowed_folders(input_dirs...; eqdsk_file, recursive=true)
+    files = ["b2fgmtry", "b2time.nc", "gridspacedesc.yml", eqdsk_file]
+    println(files)
+    output_files = fill("", length(files))
+    if recursive
+        dirs = []
+        for dir in input_dirs
+            dirs = append!(dirs, [subdir[1] for subdir in [item for item in walkdir(dir)]])
+        end
+    else
+        dirs = input_dirs
+    end
+    for i in 1:length(files)
+        for dir in dirs
+            file = dir * "/" * files[i]
+            if isfile(file)
+                output_files[i] = file
+                break
+            end
+        end
+    end
+    return output_files
+end
+
+"""
+    preparation()
+
+Gathers SOLPS and EFIT files and loads them into IMAS structure. Extrapolates
+profiles as needed to get a complete picture.
+"""
+function preparation(eqdsk_file, dirs...)
+    b2fgmtry, b2time, gridspec, eqdsk = find_files_in_allowed_folders(dirs, eqdsk_file=eqdsk_file)
+    dd = SOLPS2IMAS.solps2imas(b2gmtry, b2output, gsdesc)
+
+    # https://github.com/JuliaFusion/EFIT.jl/blob/master/src/io.jl
+    gfile = EFIT.readg(eqdsk)
+
+end
 
 end # module SD4SOLPS
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 import SD4SOLPS
+import SOLPS2IMAS
 using Plots
 using Test
 
@@ -11,14 +12,14 @@ function make_test_profile(x; sym=0.9657, hwid=0.05421, offset=0.0953, pedestal=
     return b .+ amplitude .* ((1 .+ alpha .* z) .* exp.(z) .- exp.(.-z)) ./ (exp.(z) .+ exp.(.-z))
 end
 
-function plot_test_profiles()
-    edge_rho = Array(LinRange(0.88, 1.0, 18))
-    edge_quantity = make_test_profile(edge_rho)
-    output_rho = Array(LinRange(0, 1.0, 201))
-    output_quantity = SD4SOLPS.extrapolate_core(edge_rho, edge_quantity, output_rho)
-    Plots.plot(output_rho, output_quantity)
-    Plots.plot!(edge_rho, edge_quantity, marker='.')
-end
+# function plot_test_profiles()
+#     edge_rho = Array(LinRange(0.88, 1.0, 18))
+#     edge_quantity = make_test_profile(edge_rho)
+#     output_rho = Array(LinRange(0, 1.0, 201))
+#     output_quantity = SD4SOLPS.extrapolate_core(edge_rho, edge_quantity, output_rho)
+#     Plots.plot(output_rho, output_quantity)
+#     Plots.plot!(edge_rho, edge_quantity, marker='.')
+# end
 
 @testset "core_profile_extension" begin
     edge_rho = Array(LinRange(0.88, 1.0, 18))
@@ -28,3 +29,15 @@ end
     @test length(output_quantity) == length(output_rho)
 end
 
+@testset "utilities" begin
+    sample_path = splitdir(pathof(SOLPS2IMAS))[1] * "/../samples/"
+    file_list = SD4SOLPS.find_files_in_allowed_folders(sample_path, eqdsk_file="thereisntoneyet")
+    @test length(file_list) == 4
+    b2fgmtry, b2time, gridspec, eqdsk = file_list
+    @test length(b2fgmtry) > 10
+    @test endswith(b2fgmtry, "b2fgmtry")
+    @test length(b2time) > 10
+    @test endswith(b2time, "b2time.nc")
+    @test length(gridspec) > 10
+    @test endswith(gridspec, "gridspacedesc.yml")
+end
