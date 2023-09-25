7bdf0c78e90e25e40cc71c123ccf3662647f2fff
David Eldon <eldond@fusion.gat.com>
2023-09-25 13:10:46 -0700

    Try a different way of handling unitful quantities in gas unit converter

--- src/actuator_model.jl
+++ src/actuator_model.jl
@@ -30,7 +30,8 @@ gas_unit_converter()
 
 Converts gas flows between different units. Uses ideal gas law to convert between
 Pressure * volume type flows / quantities and count / current types of units.
-Output will be unitful; take output.val if you do no want the units attached.
+There is a version that accepts floats in and outputs floats, and another that
+deals in Unitful quantities.
 """
 function gas_unit_converter(
     value_in::Float64, units_in::String, units_out::String; species::String="H", temperature=293.15,
@@ -75,33 +76,54 @@ gas_unit_converter()
 
 Converts gas flows between different units. Uses ideal gas law to convert between
 Pressure * volume type flows / quantities and count / current types of units.
-Output will be unitful; take output.val if you do no want the units attached.
+This is the Unitful version.
+Output will be unitful, but the units are not simplified automatically. You can
+perform operations such as
+    (output |> Unitful.upreferred).val
+    Unitful.uconvert(Unitful.whatever, output).val
+to handle simplification or conversion of units.
+
+Although this function pretends torr L s^-1 and Pa m^3 s^-1 are different, use of
+Unitful should cause them to behave the same way as long as you simplify or convert
+units at the end. This means that you can use other pressure*volume type gas units
+and call them torr L s^-1 and the script will deal with them up to having messy
+units in the output.
 """
 function gas_unit_converter(
-    value_in::Unitful.Quantity, units_in, units_out; species::String="H", temperature=293.15 * Unitful.K,
+    value_in::Unitful.Quantity, units_in::String, units_out::String; species::String="H", temperature=293.15 * Unitful.K,
 )
     if units_in == units_out
         return value_in
     end
-    if units_in in ["torr L s^-1", "Pa m^3 s^-1"]
-        units_in = "pressure*volume"
-    end
-    if units_out in ["torr L s^-1", "Pa m^3 s^-1"]
-        units_out = "pressure*volume"
-    end
+
+    # The conversion between Torr*L and Pa*m^-3 is the only mundane unit conversion in
+    # here. Unitful or any other unit conversion tool could handle this by itself
+    # (e.g. Unitful.uconvert), but you wouldn't need this fancy function for that.
+    # The rest of the conversions, the hard ones (that require assuming a temperature
+    # or knowing molecule structure and atomic numbers) are all implemented with
+    # multiplicative factors, so Unitful's normal conversion handling is bypassed
+    # and replaced with a multiplicative factor.
+    torrl_to_pam3 = torrl_per_pam3 * Unitful.Pa * Unitful.m^3 / Unitful.Torr / Unitful.L
+    # torrl_to_pam3 should simplify to 1, but Unitful doesn't do this simpliciation
+    # without prompting. So the Torr L or the Pa m^-3 will cancel when this factor
+    # is multiplied by some gas pressure*volume units.
 
     pam3_to_molecules =
         Unitful.J / (temperature * BoltzmannConstant) / (Unitful.Pa * Unitful.m^3)
+    torrl_to_molecules = torrl_to_pam3 * pam3_to_molecules
+
 
     factor_to_get_molecules_s = Dict(
+        "torr L s^-1" => torrl_to_molecules,
         "molecules s^-1" => 1.0,
-        "pressure*volume" => pam3_to_molecules,
+        "Pa m^3 s^-1" => pam3_to_molecules,
         "el s^-1" => 1.0 / electrons_per_molecule[species],
         "A" => ElementaryCharge / electrons_per_molecule[species],
     )
     factor_to_get_molecules = Dict(
+        "torr L" => torrl_to_molecules,
         "molecules" => 1.0,
-        "pressure*volume" => pam3_to_molecules,
+        "Pa m^3" => pam3_to_molecules,
         "el" => 1.0 / electrons_per_molecule[species],
         "C" => ElementaryCharge / electrons_per_molecule[species],
     )
